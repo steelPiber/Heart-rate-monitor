f425d82dedf9df48ca789000487c9a1d6451b3db
dobupower <117064003+dobupower@users.noreply.github.com>
2024-04-09 17:44:38 +0900

    Update server.js

--- web-server/server.js
+++ web-server/server.js
@@ -5,17 +5,16 @@ console.log('0.2.1 :1시간 BPM 평균값 오류 반환');
 const WebSocket = require('ws');
 const http = require('http');
 const express = require('express');
-const path = require('path');
 const static = require('serve-static');
 const expressWs = require('express-ws');
 
 //  oracledb.js의 함수들 불러오기
 const oracleDB = require('./oracledb.js');
+const authController = require('./authController.js');
 
 const app = express();
-app.use(express.urlencoded({extended:true})); //바꾼곳
 app.use(express.json());
-app.use('/public', static(path.join(__dirname, 'public')));
+app.use(authController);
 
 const server = http.createServer(app);
 const PORT_HTTP = 8081;
@@ -162,83 +161,6 @@ app.get('/hourly-chart', async (req, res) => {
     console.error('Error retrieving data:', err);
   }
 });
-//아이디 중복 검사
-app.get('/checkEmailDuplicate', async (req, res) => {
-  const userEmail = req.query.email; // 요청에서 사용자 이메일을 추출
-  try {
-    const Email_isDuplicate = await oracleDB.checkUserEmailExists(userEmail); // 사용자 이메일을 전달
-    res.json({ Email_isDuplicate });
-  } catch (error) {
-    console.error('중복 확인 오류:', error);
-    res.status(500).send('중복 확인 중 오류가 발생했습니다.');
-  }
-});
-//닉네임 중복 검사
-app.get('/checkNickDuplicate', async (req, res) => {
-  const userNick = req.query.nickname; // 요청에서 사용자 이메일을 추출
-  try {
-    const Nick_isDuplicate = await oracleDB.checkUserNickExists(userNick); // 사용자 이메일을 전달
-    res.json({ Nick_isDuplicate });
-  } catch (error) {
-    console.error('중복 확인 오류:', error);
-    res.status(500).send('중복 확인 중 오류가 발생했습니다.');
-  }
-});
-app.get('/checkCodeDuplicate', async (req, res) => {
-  const userEmail = req.query.email;
-  const auth_code = req.query.auth_code;
-  try {
-    const Code_isDuplicate = await oracleDB.checkMailAuth(userEmail, auth_code);
-    res.json({ Code_isDuplicate });
-  } catch (error) {
-    console.error('인증 코드 확인 오류:', error);
-    res.status(500).send('인증 코드 확인중 오류가 발생했습니다.');
-  }
-});
-
-// 회원가입 처리
-app.post('/signup', async (req, res)=> {
-    const paramEmail = req.body.email;
-    const paramname = req.body.username;
-    const paramNickname = req.body.userNick;
-    const paramauth_code = req.body.auth_code;
-    const paramPw = req.body.userpasswd;
-    console.log("paramEmail: ", paramEmail);
-  console.log("paramname: ", paramname);
-  console.log("paramNickname: ", paramNickname);
-  console.log("paramauth_code: ", paramauth_code);
-  console.log("paramPw: ", paramPw);
-    try {
-      await oracleDB.insertUser(paramEmail, paramname, paramNickname, paramauth_code, paramPw);
-      await oracleDB.insertUserlog(paramEmail, paramNickname, paramMac);
-      res.status(200).send('회원가입 성공');
-    } catch (err) {
-      await oracleDB.insertUserErrlog(paramEmail, paramNickname, paramMac);
-      res.status(500).send('회원가입 오류');
-      console.error('회원가입 오류:', err);
-    }
-});
-
-// 로그인 처리
-app.post('/signin', async (req, res)=> {
-    const paramEmail = req.body.email;
-    const paramPw = req.body.userpasswd;
-
-    try {
-        const isLoginSuccess = await oracleDB.selectUser(paramEmail, paramPw);
-        if (isLoginSuccess) {
-            await oracleDB.selectUserlog(paramEmail);
-            res.status(200).send('로그인 성공');
-        } else {
-            await oracleDB.selectUserErrlog(paramEmail);
-            res.status(401).send('이메일 또는 비밀번호가 잘못되었습니다.');
-        }
-    } catch (err) {
-        res.status(500).send('로그인 오류');
-        console.error('로그인 오류:' , err);
-    }
-});
-
 // Create a WebSocket server at port 13389
 const wss = new WebSocket.Server({ noServer: true });
 
