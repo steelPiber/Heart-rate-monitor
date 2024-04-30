731a0bbf1ba8320915085326bb5d408289899ca3
dobupower <117064003+dobupower@users.noreply.github.com>
2024-04-30 11:56:59 +0900

    Update firebase.js

--- web-server/firebase.js
+++ web-server/firebase.js
@@ -1,1 +1,17 @@
+console.log(`firebase.js 모듈 분리`);
 
+const firebase = require("firebase");
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+firebase.initializeApp(firebaseConfig)
+let firebase = firebase.database();
+
+module.exports = firebase;
