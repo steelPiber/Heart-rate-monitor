93b24877fef40ad8a112483ead60a0f7dd765a58
dobupower <117064003+dobupower@users.noreply.github.com>
2024-04-30 13:41:24 +0900

    Update google_authController.js

--- web-server/google_authController.js
+++ web-server/google_authController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const static = require('serve-static');
 const path = require('path');
-const oracleDB = require('./firebase.js');
+const firebase = require('./firebase.js');
 router.use(express.urlencoded({extended:true}));
 router.use(express.json());
 router.use('/public', static(path.join(__dirname, 'public')));
@@ -10,7 +10,7 @@ router.use('/public', static(path.join(__dirname, 'public')));
 router.post('/verify-token', async (req, res) => {
   const idToken = req.body.idToken; // 클라이언트에서 보낸 ID 토큰
   try {
-    const decodedToken = await verifyGoogleToken(idToken);
+    const decodedToken = await firebase.verifyGoogleToken(idToken);
     // 토큰 검증 성공, 메인 페이지로 리디렉션
     return res.redirect('/main');
   } catch (error) {
