8735d6018a49e5dbd2c8b564c2c7f9bc7aa80e74
dobupower <117064003+dobupower@users.noreply.github.com>
2024-04-30 13:43:00 +0900

    Update firebase.js

--- web-server/firebase.js
+++ web-server/firebase.js
@@ -1,17 +1,25 @@
 console.log(`firebase.js 모듈 분리`);
 
-const firebase = require("firebase");
+const admin = require('firebase-admin');
 
-const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-};
-firebase.initializeApp(firebaseConfig)
+var serviceAccount = require('firebase_conf.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+async function verifyGoogleToken(idToken) {
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    const uid = decodedToken.uid;
+    // 사용자 정보를 가져오거나 필요한 작업 수행
+    return decodedToken;
+  } catch (error) {
+    console.error('Error verifying Google token', error);
+    throw error;
+  }
+}
+
 let firebase = firebase.database();
 
 module.exports = firebase;
