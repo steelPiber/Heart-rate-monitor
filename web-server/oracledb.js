4b328a85580bd3b1500d77311f9ebf4b784a85d6
dobupower <117064003+dobupower@users.noreply.github.com>
2024-05-30 19:50:32 +0900

    Update oracledb.js

--- web-server/oracledb.js
+++ web-server/oracledb.js
@@ -10,7 +10,7 @@ const nodemailer = require('./mail_auth.js');
 const dbConfig = {
   user: 'piber',
   password: 'wjsansrk',
-  connectString: '202.31.243.45:1521/ORA21'
+  connectString: '202.31.243.44:1521/ORA21APEX'
 };
 
 //Oracle DB 연결확인
