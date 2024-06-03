eae7bdfc67db00c366727a6525d854eb39cb1587
dobupower <117064003+dobupower@users.noreply.github.com>
2024-06-03 14:35:08 +0900

    Update server.js

--- web-server/server.js
+++ web-server/server.js
@@ -238,7 +238,7 @@ app.get('/hourlychartquery', async (req, res) => {
 
 
 // Start the HTTP server on port 8081
-app.listen(PORT, () => {
+app.listen(PORT_HTTP, () => {
     console.log(`HTTP server is running on port ${PORT_HTTP}`);
     console.log(`server is running on port ${PORT}`);
 });
