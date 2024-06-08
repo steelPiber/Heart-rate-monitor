2f7b272d0fbe4bfd4ff8fb3479df2de5a851d2f1
dobupower <117064003+dobupower@users.noreply.github.com>
2024-06-08 17:09:53 +0900

    Update server.js

--- web-server/server.js
+++ web-server/server.js
@@ -1,3 +1,10 @@
+// 11.03 - 오라클 DB 데이터값 추출
+console.log(`VERSION_0.1_11.29`);
+console.log(`0.1 : 오라클 db 접속 및 포트 개선`);
+
+console.log(`VERSION_0.2_12.01`);
+console.log('0.2 : 1분당 평균심박수를 JSON형태로 응답');
+
 const http = require('http');
 const express = require('express');
 const path = require('path');
@@ -8,12 +15,13 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT_HTTP = 8081;
+const PORT = 13389;
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(googleAuthRouter);
-app.use('/bpm', bpmRouter); // Updated usage
+app.use('/bpm', bpmRouter); 
 
 // Static Files
 app.use(express.static(path.join(__dirname, '/dashboard')));
@@ -53,7 +61,66 @@ app.get('/hourly', (req, res) => {
   res.sendFile(__dirname + '/hourlychart.html');
 });
 
-// Start the HTTP server
+async function executeQuery(query, params) {
+  const connection = await oracleDB.connectToOracleDB();
+  try {
+    const result = await connection.execute(query, params);
+    await connection.close();
+    return result;
+  } catch (err) {
+    console.error('Error executing query:', err);
+    await connection.close();
+    throw err;
+  }
+}
+
+async function getUserEmailFromToken(req) {
+  const accessToken = req.cookies.accessToken;
+  if (!accessToken) {
+    throw new Error('Access token is missing');
+  }
+  const userInfo = await getUserInfo(accessToken);
+  return userInfo.email.split('@')[0];
+}
+
+////////////////////////////////////////////////////////////////////////////
+
+
+// Hourly chart handler
+app.get('/hourly-chart', async (req, res) => {
+  try {
+    // 사용자 이메일 정보를 가져옵니다.
+    const userEmail = await getUserEmailFromToken(req);
+
+    const query = oracleDB.everyHourDuringTheDayQuery();
+    const result = await executeQuery(query, { Email: userEmail });
+
+    // Convert the query result to an array
+    const data = result.rows.map(row => {
+      // row[0]에서 "2024-" 부분을 제거하고 월 일만 추출
+      const date = row[0].slice(5, 16); // "06-07 11"
+      const [month, day, hour] = date.split(' ');
+      const formattedDate = `${month} ${day}`;
+      return [formattedDate, row[1]];
+    });
+    
+    const responseData = {
+      userEmail: userEmail,
+      data: data
+    };
+    
+    // Send the data as JSON
+    res.json(responseData);
+  } catch (err) {
+    res.status(500).send('Error retrieving data');
+  }
+});
+
+// Start the HTTP server on port 8081
 app.listen(PORT_HTTP, () => {
-  console.log(`HTTP server is running on port ${PORT_HTTP}`);
+    console.log(`HTTP server is running on port ${PORT_HTTP}`);
+});
+// Start the HTTP server on port 8081
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
 });
