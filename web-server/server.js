29f1acb8108a6fac6302bc5e2bc3c2a6e14658d1
dobupower <117064003+dobupower@users.noreply.github.com>
2024-06-08 17:16:50 +0900

    Update server.js

--- web-server/server.js
+++ web-server/server.js
@@ -1,64 +1,57 @@
-// 11.03 - 오라클 DB 데이터값 추출
-console.log(`VERSION_0.1_11.29`);
-console.log(`0.1 : 오라클 db 접속 및 포트 개선`);
-
-console.log(`VERSION_0.2_12.01`);
-console.log('0.2 : 1분당 평균심박수를 JSON형태로 응답');
-
 const http = require('http');
 const express = require('express');
 const path = require('path');
-const { router: googleAuthRouter, getUserInfo } = require("./google_authController.js");
-const bpmRouter = require('./bpm.js'); // Updated import
-const oracleDB = require('./oracledb.js');
 const cookieParser = require('cookie-parser');
+const oracleDB = require('./oracledb.js');
+const { router: googleAuthRouter, getUserInfo } = require("./google_authController.js");
+const bpmRouter = require('./bpm.js'); // bpm 라우터 모듈 불러오기
 
 const app = express();
-const PORT_HTTP = 8081;
-const PORT = 13389;
 
-// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(googleAuthRouter);
-app.use('/bpm', bpmRouter); 
+app.use(bpmRouter); // bpm 라우터 사용
 
-// Static Files
+// 정적 파일 미들웨어를 사용하여 CSS, 이미지, JS 등의 정적 파일 제공
 app.use(express.static(path.join(__dirname, '/dashboard')));
+const server = http.createServer(app);
+const PORT_HTTP = 8081;
+const PORT = 13389;
 
-// Oracle DB Connection
+// Oracle DB 연결 확인
 oracleDB.connectToOracleDB()
   .then(connection => {
+    // 연결 성공 시 동작
     console.log('Oracle DB 연결 성공');
+    // 여기서 필요한 작업 수행
   })
   .catch(error => {
+    // 연결 실패 시 동작
     console.error('Oracle DB 연결 실패:', error);
   });
 
-// Serve HTML pages
+// Serve HTML page at port 8081
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'heart-dashboard', 'index.html'));
+    res.sendFile(path.join(__dirname, 'heart-dashboard', 'index.html'));
 });
-
 app.get('/login/:userEmailWithoutDomain', (req, res) => {
-  const userEmailWithoutDomain = req.params.userEmailWithoutDomain;
-  const accessToken = req.query.access_token;
-  console.log('로그인 토큰', accessToken);
-
-  if (!accessToken) {
-    return res.status(400).send('Access token is missing');
-  }
-
-  res.cookie('accessToken', accessToken);
-  res.sendFile(path.join(__dirname, 'dashboard/pages', 'dashboard.html'));
+    const userEmailWithoutDomain = req.params.userEmailWithoutDomain;
+    res.sendFile(path.join(__dirname, 'dashboard/pages', 'dashboard.html'));
+    const accessToken = req.query.access_token; // 클라이언트에서 access token을 쿼리 파라미터로 전달
+    console.log('로그인 토큰', accessToken);
+    if (!accessToken) {
+      res.status(400).send('Access token is missing');
+      return;
+    }
+    res.cookie('accessToken', accessToken);
+    
 });
-
 app.get('/min1', (req, res) => {
-  res.sendFile(__dirname + '/min1.html');
+    res.sendFile(__dirname + '/min1.html');
 });
-
 app.get('/hourly', (req, res) => {
-  res.sendFile(__dirname + '/hourlychart.html');
+    res.sendFile(__dirname + '/hourlychart.html');
 });
 
 async function executeQuery(query, params) {
@@ -83,22 +76,15 @@ async function getUserEmailFromToken(req) {
   return userInfo.email.split('@')[0];
 }
 
-////////////////////////////////////////////////////////////////////////////
-
-
 // Hourly chart handler
 app.get('/hourly-chart', async (req, res) => {
   try {
-    // 사용자 이메일 정보를 가져옵니다.
     const userEmail = await getUserEmailFromToken(req);
-
     const query = oracleDB.everyHourDuringTheDayQuery();
     const result = await executeQuery(query, { Email: userEmail });
 
-    // Convert the query result to an array
     const data = result.rows.map(row => {
-      // row[0]에서 "2024-" 부분을 제거하고 월 일만 추출
-      const date = row[0].slice(5, 16); // "06-07 11"
+      const date = row[0].slice(5, 16); // "2024-" 부분을 제거하고 월 일만 추출
       const [month, day, hour] = date.split(' ');
       const formattedDate = `${month} ${day}`;
       return [formattedDate, row[1]];
@@ -109,7 +95,6 @@ app.get('/hourly-chart', async (req, res) => {
       data: data
     };
     
-    // Send the data as JSON
     res.json(responseData);
   } catch (err) {
     res.status(500).send('Error retrieving data');
@@ -120,7 +105,8 @@ app.get('/hourly-chart', async (req, res) => {
 app.listen(PORT_HTTP, () => {
     console.log(`HTTP server is running on port ${PORT_HTTP}`);
 });
-// Start the HTTP server on port 8081
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
+
+module.exports = { executeQuery, getUserEmailFromToken };
