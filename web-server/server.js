9d7a179039ddfbe426745cb441f3cdeb0b374929
dobupower <117064003+dobupower@users.noreply.github.com>
2024-06-08 16:55:12 +0900

    Update server.js

--- web-server/server.js
+++ web-server/server.js
@@ -10,6 +10,7 @@ const express = require('express');
 const static = require('serve-static');
 const path = require('path');
 const { router: googleAuthRouter, getUserInfo } = require("./google_authController.js");
+const bpmjs = require('./bpm.js');
 
 //  oracledb.js의 함수들 삽입 
 const oracleDB = require('./oracledb.js'); // oracledb.js 파일 경로에 따라 수정
@@ -20,6 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(googleAuthRouter);
+app.use(bpmjs);
 
 // 정적 파일 미들웨어를 사용하여 CSS, 이미지, JS 등의 정적 파일 제공
 app.use(express.static(path.join(__dirname, '/dashboard')));
@@ -39,23 +41,27 @@ oracleDB.connectToOracleDB()
     console.error('Oracle DB 연결 실패:', error);
   });
 
-app.post("/data", async (req, res) => {
-    console.log("Received request body:", req.body); // 요청 본문 전체를 출력하여 디버그
-    const { bpm, tag, email } = req.body;
-    console.log(
-      `Received data - BPM: ${bpm}, Tag: ${tag}, Email: ${email}`,
-    );
-    const EmailWithoutDomain = email.split('@')[0];
-    try {
-    const result = await oracleDB.insertBPMData(bpm, EmailWithoutDomain, tag);
-    console.log('Successfully inserted BPM data into Oracle DB');
-    res.sendStatus(200);
-  } catch (error) {
-    console.error('Error inserting BPM data into Oracle DB:', error);
-    res.sendStatus(500);
+async function executeQuery(query, params) {
+  const connection = await oracleDB.connectToOracleDB();
+  try {
+    const result = await connection.execute(query, params);
+    await connection.close();
+    return result;
+  } catch (err) {
+    console.error('Error executing query:', err);
+    await connection.close();
+    throw err;
   }
-});
+}
 
+async function getUserEmailFromToken(req) {
+  const accessToken = req.cookies.accessToken;
+  if (!accessToken) {
+    throw new Error('Access token is missing');
+  }
+  const userInfo = await getUserInfo(accessToken);
+  return userInfo.email.split('@')[0];
+}
 
 // Serve HTML page at port 8081
 app.get('/', (req, res) => {
@@ -82,151 +88,6 @@ app.get('/hourly', (req, res) => {
 
 
 ////////////////////////////////////////////////////////////////////////////
-
-
-
-async function executeQuery(query, params) {
-  const connection = await oracleDB.connectToOracleDB();
-  try {
-    const result = await connection.execute(query, params);
-    await connection.close();
-    return result;
-  } catch (err) {
-    console.error('Error executing query:', err);
-    await connection.close();
-    throw err;
-  }
-}
-
-async function getUserEmailFromToken(req) {
-  const accessToken = req.cookies.accessToken;
-  if (!accessToken) {
-    throw new Error('Access token is missing');
-  }
-  const userInfo = await getUserInfo(accessToken);
-  return userInfo.email.split('@')[0];
-}
-
-// Realtime query handler
-app.get('/realtime-bpm', async (req, res) => {
-  try {
-    const userEmail = await getUserEmailFromToken(req);
-    const query = oracleDB.realtimeQuery();
-    const result = await executeQuery(query, { Email: userEmail });
-
-    const data = result.rows.map(row => row[0]);
-    res.json(data);
-  } catch (err) {
-    res.status(500).send('Error retrieving data');
-  }
-});
-
-// Min query handler
-app.get('/average-bpm', async (req, res) => {
-  try {
-    // 사용자 이메일 정보를 가져옵니다.
-    const userEmail = await getUserEmailFromToken(req);
-    const query = oracleDB.minQuery();
-    const result = await executeQuery(query, { Email: userEmail });
-
-    // Convert the query result to an array
-    const data = result.rows.map(row => row[0]);
-
-    // Send the data as JSON
-    res.json(data);
-  } catch (err) {
-    res.status(500).send('Error retrieving data');
-  }
-});
-
-// Hour query handler
-app.get('/hour-bpm', async (req, res) => {
-  try {
-    // 사용자 이메일 정보를 가져옵니다.
-    const userEmail = await getUserEmailFromToken(req);
-    const query = oracleDB.hourQuery();
-    const result = await executeQuery(query, { Email: userEmail });
-
-    // Convert the query result to an array
-    const data = result.rows.map(row => row[0]);
-
-    // Send the data as JSON
-    res.json(data);
-  } catch (err) {
-    res.status(500).send('Error retrieving data');
-  }
-});
-
-// Day query handler
-app.get('/day-bpm', async (req, res) => {
-  try {
-    // 사용자 이메일 정보를 가져옵니다.
-    const userEmail = await getUserEmailFromToken(req);
-    const query = oracleDB.dayQuery();
-    const result = await executeQuery(query, { Email: userEmail });
-
-    // Convert the query result to an array
-    const data = result.rows.map(row => row[0]);
-
-    // Send the data as JSON
-    res.json(data);
-  } catch (err) {
-    res.status(500).send('Error retrieving data');
-  }
-});
-// Week query handler
-app.get('/week-bpm', async (req, res) => {
-  try {
-    // 사용자 이메일 정보를 가져옵니다.
-    const userEmail = await getUserEmailFromToken(req);
-    const query = oracleDB.weekQuery();
-    const result = await executeQuery(query, { Email: userEmail });
-
-    // Convert the query result to an array
-    const data = result.rows.map(row => row[0]);
-
-    // Send the data as JSON
-    res.json(data);
-  } catch (err) {
-    res.status(500).send('Error retrieving data');
-  }
-});
-
-// Month query handler
-app.get('/month-bpm', async (req, res) => {
-  try {
-    // 사용자 이메일 정보를 가져옵니다.
-    const userEmail = await getUserEmailFromToken(req);
-    const query = oracleDB.monthQuery();
-    const result = await executeQuery(query, { Email: userEmail });
-
-    // Convert the query result to an array
-    const data = result.rows.map(row => row[0]);
-
-    // Send the data as JSON
-    res.json(data);
-  } catch (err) {
-    res.status(500).send('Error retrieving data');
-  }
-});
-
-// Year query handler
-app.get('/year-bpm', async (req, res) => {
-  try {
-    // 사용자 이메일 정보를 가져옵니다.
-    const userEmail = await getUserEmailFromToken(req);
-    const query = oracleDB.yearQuery();
-    const result = await executeQuery(query, { Email: userEmail });
-
-    // Convert the query result to an array
-    const data = result.rows.map(row => row[0]);
-
-    // Send the data as JSON
-    res.json(data);
-  } catch (err) {
-    res.status(500).send('Error retrieving data');
-  }
-});
 
 // Hourly chart handler
 app.get('/hourly-chart', async (req, res) => {
