7e092afbc3ca88b9aa343a30520d056f544c69f3
dobupower <117064003+dobupower@users.noreply.github.com>
2024-06-09 06:30:57 +0900

    Update server.js

--- web-server/server.js
+++ web-server/server.js
@@ -82,7 +82,7 @@ app.get('/hourly-chart', async (req, res) => {
 app.get('/weekly-chart', async (req, res) => {
   try {
     const userEmail = await getUserEmailFromToken(req);
-    const query = oracleDB.everySevenHourDuringTheWeekQuery();
+    const query = oracleDB.everyHourDuringTheDayQuery();
     const result = await executeQuery(query, { Email: userEmail });
 
     const data = result.rows.map(row => {
