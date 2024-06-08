bbc701e070a5b4db1abec1c69e17a0b3f13bf786
dobupower <117064003+dobupower@users.noreply.github.com>
2024-06-08 17:21:52 +0900

    Update server.js

--- web-server/server.js
+++ web-server/server.js
@@ -3,8 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const oracleDB = require('./oracledb.js');
-const { router: googleAuthRouter, getUserInfo } = require("./google_authController.js");
+const { router: googleAuthRouter } = require("./google_authController.js");
 const bpmRouter = require('./bpm.js'); // bpm 라우터 모듈 불러오기
+const { executeQuery, getUserEmailFromToken } = require('./utility.js'); // 유틸리티 함수들 사용
 
 const app = express();
 
@@ -22,59 +23,33 @@ const PORT = 13389;
 // Oracle DB 연결 확인
 oracleDB.connectToOracleDB()
   .then(connection => {
-    // 연결 성공 시 동작
     console.log('Oracle DB 연결 성공');
-    // 여기서 필요한 작업 수행
   })
   .catch(error => {
-    // 연결 실패 시 동작
     console.error('Oracle DB 연결 실패:', error);
   });
 
 // Serve HTML page at port 8081
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'heart-dashboard', 'index.html'));
+  res.sendFile(path.join(__dirname, 'heart-dashboard', 'index.html'));
 });
 app.get('/login/:userEmailWithoutDomain', (req, res) => {
-    const userEmailWithoutDomain = req.params.userEmailWithoutDomain;
-    res.sendFile(path.join(__dirname, 'dashboard/pages', 'dashboard.html'));
-    const accessToken = req.query.access_token; // 클라이언트에서 access token을 쿼리 파라미터로 전달
-    console.log('로그인 토큰', accessToken);
-    if (!accessToken) {
-      res.status(400).send('Access token is missing');
-      return;
-    }
-    res.cookie('accessToken', accessToken);
-    
+  const userEmailWithoutDomain = req.params.userEmailWithoutDomain;
+  res.sendFile(path.join(__dirname, 'dashboard/pages', 'dashboard.html'));
+  const accessToken = req.query.access_token;
+  console.log('로그인 토큰', accessToken);
+  if (!accessToken) {
+    res.status(400).send('Access token is missing');
+    return;
+  }
+  res.cookie('accessToken', accessToken);
 });
 app.get('/min1', (req, res) => {
-    res.sendFile(__dirname + '/min1.html');
+  res.sendFile(__dirname + '/min1.html');
 });
 app.get('/hourly', (req, res) => {
-    res.sendFile(__dirname + '/hourlychart.html');
+  res.sendFile(__dirname + '/hourlychart.html');
 });
-
-async function executeQuery(query, params) {
-  const connection = await oracleDB.connectToOracleDB();
-  try {
-    const result = await connection.execute(query, params);
-    await connection.close();
-    return result;
-  } catch (err) {
-    console.error('Error executing query:', err);
-    await connection.close();
-    throw err;
-  }
-}
-
-async function getUserEmailFromToken(req) {
-  const accessToken = req.cookies.accessToken;
-  if (!accessToken) {
-    throw new Error('Access token is missing');
-  }
-  const userInfo = await getUserInfo(accessToken);
-  return userInfo.email.split('@')[0];
-}
 
 // Hourly chart handler
 app.get('/hourly-chart', async (req, res) => {
@@ -103,10 +78,8 @@ app.get('/hourly-chart', async (req, res) => {
 
 // Start the HTTP server on port 8081
 app.listen(PORT_HTTP, () => {
-    console.log(`HTTP server is running on port ${PORT_HTTP}`);
+  console.log(`HTTP server is running on port ${PORT_HTTP}`);
 });
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+  console.log(`server is running on port ${PORT}`);
 });
-
-module.exports = { executeQuery, getUserEmailFromToken };
