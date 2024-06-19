8a61d283756bcf4f3a6ce947bfe5a23177294dba
dobupower <117064003+dobupower@users.noreply.github.com>
2024-06-19 11:17:42 +0900

    Update chartsBT.js

--- dashboard/assets/js/chartsBT.js
+++ dashboard/assets/js/chartsBT.js
@@ -1,58 +1,56 @@
 // 비트트랙 운동심박수 차트 그리기 스크립트 (선 라인)
 function workoutChart(url) {
-    const canvases = document.querySelectorAll('.workout-graph');
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        if (data.length > 0) {
-            const latestData = data[data.length - 1]; // 가장 마지막 데이터를 선택
-            console.log('Latest data:', latestData);
-            const segmentHeartRateEntries = latestData.segmentHeartRateEntries;
-    
-            // x, y 값을 배열로 추출
-            const labels = segmentHeartRateEntries.map(entry => entry.x);
-            const heartRates = segmentHeartRateEntries.map(entry => entry.y);
+        .then(response => response.json())
+        .then(data => {
+            if (data.length > 0) {
+                const latestData = data[data.length - 1]; // 가장 마지막 데이터를 선택
+                console.log('Latest data:', latestData);
+                const segmentHeartRateEntries = latestData.segmentHeartRateEntries;
 
-            // Chart.js를 이용해 그래프 그리기
-            const ctx = document.getElementById('heartRateChart').getContext('2d');
-            const heartRateChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: 'Segment Heart Rate',
-                        data: heartRates,
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                        fill: false,
-                    }]
-                },
-                options: {
-                    scales: {
-                        x: {
-                            title: {
-                                display: true,
-                                text: 'Segment'
-                            }
-                        },
-                        y: {
-                            title: {
-                                display: true,
-                                text: 'Heart Rate'
+                // x, y 값을 배열로 추출
+                const labels = segmentHeartRateEntries.map(entry => entry.x);
+                const heartRates = segmentHeartRateEntries.map(entry => entry.y);
+
+                // Chart.js를 이용해 그래프 그리기
+                const ctx = document.getElementById('heartRateChart').getContext('2d');
+                const heartRateChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: 'Segment Heart Rate',
+                            data: heartRates,
+                            borderColor: 'rgba(75, 192, 192, 1)',
+                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                            fill: false,
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            x: {
+                                title: {
+                                    display: true,
+                                    text: 'Segment'
+                                }
+                            },
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: 'Heart Rate'
+                                }
                             }
                         }
                     }
-                }
-            });
-        } else {
-            console.error('No data found or data is not an array');
-        }
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+                });
+            } else {
+                console.error('No data found or data is not an array');
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 }
-workoutChart();
 
 // 비트트랙 적정운동량 차트 그리기 스크립트 (가로 막대)
 function targetZoneChart() {
@@ -60,14 +58,14 @@ function targetZoneChart() {
     canvases.forEach(canvas => {
         const ctx = canvas.getContext('2d');
         let avgBPMs;
-        const labels = ['Maximum','Hard','Moderate','Light','Warm up'];
-        avgBPMs = [10,20,40,70,100];
+        const labels = ['Maximum', 'Hard', 'Moderate', 'Light', 'Warm up'];
+        avgBPMs = [10, 20, 40, 70, 100];
 
         const backgroundColors = [
-            '#F3463A','#FF695F','#FF8880','#FF9E97','#FFBEB9'
+            '#F3463A', '#FF695F', '#FF8880', '#FF9E97', '#FFBEB9'
         ]; // 막대 색상
         const yLabelColors = [
-            '#F3463A','#FF695F','#FF8880','#FF9E97','#FFBEB9'
+            '#F3463A', '#FF695F', '#FF8880', '#FF9E97', '#FFBEB9'
         ]; // y축 라벨 색상
 
         new Chart(ctx, {
@@ -110,7 +108,7 @@ function targetZoneChart() {
                         ticks: {
                             display: true,
                             padding: 10,
-                            color: function(context){
+                            color: function (context) {
                                 return yLabelColors[context.index] || '#000';
                             },
                             font: {
@@ -143,7 +141,7 @@ function targetZoneChart() {
                     },
                 },
             },
-            plugins: [{     //y축 라벨 제자리에 있도록 하는 코드 (건들면 모양안이뻐짐)
+            plugins: [{     // y축 라벨 제자리에 있도록 하는 코드 (건들면 모양 안 예뻐짐)
                 afterDraw: chart => {
                     const yScale = chart.scales.y;
                     const tickCount = yScale.ticks.length;
@@ -167,8 +165,7 @@ function targetZoneChart() {
     });
 }
 
-targetZoneChart();
-
 document.addEventListener('DOMContentLoaded', () => {
     workoutChart('/training-record/records');
+    targetZoneChart();
 });
