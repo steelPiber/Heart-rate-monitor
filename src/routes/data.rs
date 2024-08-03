9a7f9888d299d7a3e58f6c9f2de3108d39c7f4a7
still_piber <43775386+steelPiber@users.noreply.github.com>
2024-08-03 20:51:15 +0900

    Update data.rs

--- src/routes/data.rs
+++ src/routes/data.rs
@@ -19,10 +19,11 @@ struct Data {
 async fn handle_post(Json(payload): Json<Data>, client: Arc<Client>) -> impl IntoResponse {
     println!("Received request body: {:?}", payload);
 
-    // BPM 값이 숫자인지 확인
-    if payload.bpm.parse::<i32>().is_err() {
-        return (StatusCode::BAD_REQUEST, "Invalid BPM value").into_response();
-    }
+    // BPM 값이 숫자인지 확인하고 변환
+    let bpm = match payload.bpm.parse::<i32>() {
+        Ok(bpm) => bpm,
+        Err(_) => return (StatusCode::BAD_REQUEST, "Invalid BPM value").into_response(),
+    };
 
     // 이메일 형식 확인
     let email_without_domain = match payload.email.split('@').next() {
@@ -30,7 +31,7 @@ async fn handle_post(Json(payload): Json<Data>, client: Arc<Client>) -> impl Int
         None => return (StatusCode::BAD_REQUEST, "Invalid email format").into_response(),
     };
 
-    match insert_bpm_data(&client, &payload.bpm, &email_without_domain, &payload.tag).await {
+    match insert_bpm_data(&client, bpm, &email_without_domain, &payload.tag).await {
         Ok(_) => {
             println!("Successfully inserted BPM data into PostgreSQL");
             (StatusCode::OK, "Request received").into_response()
@@ -42,7 +43,7 @@ async fn handle_post(Json(payload): Json<Data>, client: Arc<Client>) -> impl Int
     }
 }
 
-async fn insert_bpm_data(client: &Client, bpm: &str, email: &str, tag: &str) -> Result<(), tokio_postgres::Error> {
+async fn insert_bpm_data(client: &Client, bpm: i32, email: &str, tag: &str) -> Result<(), tokio_postgres::Error> {
     let insert_sql = "INSERT INTO bpmdata (BPM, EMAIL, TAG, TIME) VALUES ($1, $2, $3, $4)";
     let now = chrono::Utc::now().naive_utc(); // 시간 변환
     client.execute(insert_sql, &[&bpm, &email, &tag, &now]).await?;
