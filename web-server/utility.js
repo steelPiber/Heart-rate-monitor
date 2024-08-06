3e153db44a3df0174003a22c94f473186b165fe8
dobupower <117064003+dobupower@users.noreply.github.com>
2024-08-06 18:22:34 +0900

    Update utility.js

--- web-server/utility.js
+++ web-server/utility.js
@@ -14,17 +14,6 @@ async function executeQuery(query, params) {
   }
 }
 
-async function getUserEmailFromToken(req) {
-  const accessToken = req.cookies.accessToken; // 요청 쿠키에서 액세스 토큰 추출
-  if (!accessToken) {
-    throw new Error('Access token is missing'); // 액세스 토큰이 없으면 오류 발생
-  }
-  const userInfo = await getUserInfo(accessToken); // 액세스 토큰을 사용하여 사용자 정보 가져오기
-  console.log('userInfo : ', userInfo.email.split('@')[0]); // 이메일 도메인 부분을 제거한 사용자 이메일 로그 출력
-  return userInfo.email.split('@')[0]; // 이메일의 도메인 부분을 제거한 이메일 반환
-}
-
 module.exports = {
   executeQuery,
-  getUserEmailFromToken,
 };
