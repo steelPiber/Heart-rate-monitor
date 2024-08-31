650bfb998db653ae65ba0739d6fdcddb3a644f1b
root <root@debian.localhost>
2024-08-31 22:58:20 +0900

    Remove old heart-server-rust folder

--- heart-server-rust/src/routes/hrt/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod day_hrt; //하루 전 평균 심박수 모듈
-pub mod hour_hrt; //-2시간 전 평균 심박수 모듈
-pub mod min_hrt; //-2분전 평균  심박수 모듈
-pub mod month_hrt; //한달 전 평균 심박수 모듈
-pub mod realhrt; //실시간 심박수 모듈
-pub mod week_hrt;
-pub mod year_hrt; //0년 전 평균 심박수 몯 //일주일 전 평균 심박수 모
